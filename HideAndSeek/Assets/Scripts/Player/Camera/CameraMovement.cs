a832b9cdcba03e17ff455f1fa25e270504857f83
Maia.dev <asmodeoanxy@gmail.com>
2026-04-27 14:17:08 -0300

    Organización + cambios minimos

--- HideAndSeek/Assets/Scripts/Player/Camera/CameraMovement.cs
+++ HideAndSeek/Assets/Scripts/Player/Camera/CameraMovement.cs
@@ -7,12 +7,7 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Transform camTarget;
     public float posSpeed = .02f;
     public float rotSpeed = .05f;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position,camTarget.position, posSpeed);
