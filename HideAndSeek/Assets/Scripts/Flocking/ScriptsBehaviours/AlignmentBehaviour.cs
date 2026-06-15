8be4fe1a92239d6e4e2a53e1fbcae7b02c3a0670
Julieta <iparjuli@gmail.com>
2026-06-15 20:35:42 -0300

    Add: more comments on flocking scripts

--- HideAndSeek/Assets/Scripts/Flocking/ScriptsBehaviours/AlignmentBehaviour.cs
+++ HideAndSeek/Assets/Scripts/Flocking/ScriptsBehaviours/AlignmentBehaviour.cs
@@ -1,24 +1,26 @@
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Flocking/Behaviour/Alignment")]
-public class AlignmentBehaviour : BoidBehaviour
+public class AlignmentBehaviour : BoidBehaviour // alineacion
 {
     public override Vector3 CalculateForce(Boid boid, Collider[] boidsInRange, float radius)
     {
         Vector3 avgVelocity = Vector3.zero;
-        int cont = 0;
+        int cont = 0; // contador de vecinos validos detectados en rango
 
-        for (int i = 0; i < boidsInRange.Length; i++)
+        for (int i = 0; i < boidsInRange.Length; i++) // recorre la lista de boids en radius
         {
             var currentBoid = boidsInRange[i].GetComponent<Boid>();
-            if (currentBoid == null || currentBoid == boid) continue;
+            if (currentBoid == null || currentBoid == boid) continue; // si no hay boid o es el mismo boid, ignorar
 
-            avgVelocity += currentBoid.transform.forward;
+            avgVelocity += currentBoid.transform.forward; // lo mueve para adelante
             cont++;
         }
 
         if (cont == 0) return Vector3.zero;
 
-        return boid.CalculateSteering(avgVelocity.normalized * boid.MaxSpeed);
+        avgVelocity /= cont; // saca promedio de direccion
+
+        return boid.CalculateSteering(avgVelocity.normalized * boid.MaxSpeed); // moverse a esa posicion
     }
 }
